ae596060aa2921a4a012bf0accc84de5557d4e64
David Pires <davidpiresduarte@gmail.com>
2022-04-18 21:12:41 -0300

    adicionado página produto
    página do produto estática, porém com JavaScript para contar a quantidade dos produtos para compra

---
+++ produto.js
@@ -0,0 +1,13 @@
+
+
+function process(quant){
+  var value = parseInt(document.getElementById("quant").value);
+  value+=quant;
+  if(value < 1){
+    document.getElementById("quant").value = 1;
+  }else{
+  document.getElementById("quant").value = value;
+  }
+}
+
+
