0e71a58550665beb1e1e8fe96e77db3effd75d71
Maximiliano Muñoz Cavalcanti <95191637+maxmcavalcanti@users.noreply.github.com>
2022-04-20 10:51:17 -0300

    Adicionado responsividade a página de produto especifico

--- app/app.js
+++ app/app.js
@@ -8,8 +8,3 @@ setInterval(function () {
     counter = 1;
   }
 }, 5000);
-
-$(".produto-galeria-index").click(function () {
-  window.location = $(this).find("a").attr("href");
-  return false;
-});
